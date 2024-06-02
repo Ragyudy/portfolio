981de92de1dd6888b33da406a34aed58c8196cd7
ragyudy <gradyzyu@gmail.com>
2024-06-02 02:04:37 -0400

    navbar without links

--- grady-portfolio/src/components/NavBar.tsx
+++ grady-portfolio/src/components/NavBar.tsx
@@ -1,9 +1,21 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 function NavBar() {
   return (
-    <Navbar>
-      <Navbar.Brand href="/">Grady Yu</Navbar.Brand>
+    <Navbar expand="lg">
+      <Container>
+        {/* <Navbar.Brand href="/">:D</Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>Education</Nav.Link>
+            <Nav.Link>Experience</Nav.Link>
+            <Nav.Link>Projects</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
