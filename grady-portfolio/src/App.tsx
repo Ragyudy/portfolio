32555ba2ddc3ef19156025558d0b55ab75c56876
ragyudy <gradyzyu@gmail.com>
2024-08-20 22:06:57 -0400

    npm run build

--- grady-portfolio/src/App.tsx
+++ grady-portfolio/src/App.tsx
@@ -3,8 +3,6 @@ import GlobalStyle from "./GlobalStyle";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
-import Education from "./components/Education";
-import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 
 function App() {
@@ -32,8 +30,6 @@ function App() {
       />
       <Home />
       <About ref={aboutRef} />
-      {/* <Education /> */}
-      {/* <Experience /> */}
       <Projects ref={projectsRef} />
     </>
   );
