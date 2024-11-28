fdf925edbec5991635b9142c2112beb6f29a728e
ragyudy <gradyzyu@gmail.com>
2024-11-28 14:10:12 -0500

    update about and media queries

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,11 +5,12 @@ export function About() {
       <div className="about-content">
         <div className="about-description">
           <p>
-            Hi, I'm Grady, a sophomore at Yale studying Computer Science. I'm
-            currently a student researcher at the{" "}
+            Hi, my name is Grady. I was born and raised in NYC and am currently
+            studying Computer Science at Yale. I'm a student researcher at the{" "}
             <a href="https://vision.cs.yale.edu/">Yale Vision Laboratory</a>,
-            focusing on all-weather image removal techniques. As a developer, I
-            contribute to <a href="https://yaleclubs.info/">Yale Clubs</a> and{" "}
+            where I'm learning about all-weather image removal techniques. As a
+            developer, I contribute to{" "}
+            <a href="https://yaleclubs.info/">Yale Clubs</a> and{" "}
             <a href="https://yalecomputersociety.org/products/roomadvisor">
               RoomAdvisor
             </a>{" "}
@@ -21,10 +22,20 @@ export function About() {
             Spring and Fall 2024.
           </p>
           <p>
-            Beyond computer science, I like fingerstyle guitar, hiking, turtles,
+            I love learning and am always excited for new opportunities to
+            expand my technical expertise. My current areas of interest are in
+            full-stack development, machine learning, and, more recently,
+            computer graphics. Some of my favorite animated films include
+            Spirited Away, The Good Dinosaur, and WALL-E.
+          </p>
+          <p>
+            I enjoy teaching as much as I do learning! I'm currently a teaching
+            assistant for CS 201 (Introduction to Computer Science) at Yale.
+          </p>
+          <p>
+            Beyond computer science, I'm passionate about{" "}
             <a href="https://www.instagram.com/yalebadminton/"> badminton</a>,
-            friends, food, and a bunch of other things :D. I'm always excited
-            for new opportunities to learn and grow!
+            turtles, fingerstyle guitar, hiking, and food!
           </p>
         </div>
       </div>
